f261066b5e5f5b9b3104e13740ee7f9db6dc746e
Akashra22 <akashvelayutham.22@gmail.com>
2022-07-21 23:43:37 +0530

    Script update

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -5,57 +5,57 @@ from tkinter import messagebox as mb
 
 root = Tk()
 root.title("Youtube downloader")
-root.geometry('500x400')# This gives the size of the tkinter wizard.
-root.resizable(0, 0)# With this you cannot maximise and minimise the tkinter wizard.
+root.geometry('500x400')
+root.resizable(0, 0)
 
-Default = os.path.abspath( os.path.dirname( __file__ ))# This say the absolute directory of the script.
+Default = os.path.abspath( os.path.dirname( __file__ ))
 
 def video_download():
     url = YouTube(str(link.get())) #This captures the link(url) and locates it from YouTube.
-    video = url.streams.first()# This captures the streams available for downloaded for the video.
-    mp4_format = url.streams.filter(file_extension="mp4")# This captures the streams of the video in .mp4 format.
-    high_resol_files = mp4_format.get_highest_resolution()# This captures the streams of the video in high resolution.
+    video = url.streams.first()# This captures the streams available for downloaded for the video i.e. 360p, 720p, 1080p. etc.
+    mp4_format = url.streams.filter(file_extension="mp4")
+    high_resol_files = mp4_format.get_highest_resolution()
     #print(url.streams)
     #print(url.streaming_data)
     #print(url.streams.get_highest_resolution().resolution)
-    high_resol_files.download(Default) # This download the video
-    Label2 = Label(root, text='Video saved in location:  ' + Default)
+    high_resol_files.download(Default) # This is the method with the instruction to download the video.
+    Label2 = Label(root, text='Video saved location:  ' + Default, font="calibri 10")
     Label2.place(x=40, y=260)
-    Label3 = Label(root, text='Video resolution:  ' + url.streams.get_highest_resolution().resolution)
+    Label3 = Label(root, text='Video resolution:  ' + url.streams.get_highest_resolution().resolution, font="calibri 10")
     Label3.place(x=40, y=240)
-    label4 = Label(root, text='Video name: '+ url.title)
+    label4 = Label(root, text='Video name: '+ url.title, font="calibri 10")
     label4.place(x=40, y=220)
-    Label(root, text="Video downloaded", font="calibri 12").place(x=48, y=120) #Once the video is downloaded, this label `downloaded` is displayed to show dowload completion.
+    Label(root, text="Video downloaded", font="calibri 12").place(x=40, y=135) #Once the video is downloaded, this label `downloaded` is displayed to show dowload completion.
 
 def audio_download():
     res = mb.askquestion('Confirm', 'Do you want to download Audio only')      
     if res == 'yes' :
         url = YouTube(str(link.get())) 
-        audio_only = url.streams.filter(only_audio=True).first()# This only stream audio of the video.
-        audio_file = audio_only.download(Default)# This download the stream in audio format.
-        Label6 = Label(root, text='Audio saved in location:  ' + Default)
+        audio_only = url.streams.filter(only_audio=True).first()
+        down = audio_only.download(Default)
+        Label6 = Label(root, text='Audio saved location:  ' + Default, font="calibri 10")
         Label6.place(x=40, y=300)
-        label7 = Label(root, text='Audio name: '+ url.title)
+        label7 = Label(root, text='Audio name: '+ url.title, font="calibri 10")
         label7.place(x=40, y=320)
         Label5 = Label(root, text="Audio downloaded", font="calibri 12")
-        Label5.place(x=318, y=120)
-        base, ext = os.path.splitext(audio_file)
-        new_file = base + '.mp3'# This convert the audio file to .mp3 format.
-        os.rename(audio_file, new_file)# This makes the audio file to save in .mp3 format.
+        Label5.place(x=320, y=135)
+        base, ext = os.path.splitext(down)
+        new_file = base + '.mp3'
+        os.rename(down, new_file)
     else :
-        root.destroy()
+        root.destroy() 
 
 Label(root, text="Youtube downloader", font='calibri 13 bold').pack()
 link = StringVar() # Specifying the variable type
 Label(root, text="Paste URL", font='calibri 14 bold').place(x=200, y=55)
-link_enter = Entry(root, width=70, textvariable=link).place(x=30, y=85)# The input box to get the input data.
+link_enter = Entry(root, width=70, textvariable=link).place(x=35, y=95)
 
-Button1 = Button(root, text='Video Downloader', font='calibri 14 bold', 
-bg='blue', padx=2,command= video_download)# The button to download the video.
+video_icon = PhotoImage(file = r"File_Directory\video.png")
+music_icon = PhotoImage(file = r"File_Directory\music.png")
+Button1 = Button(root, text= ' Video Download', font = 'calibri 10 bold', image = video_icon, command = video_download, compound = LEFT, bg = '#0059b3', fg = 'white')
 Button1.place(x=40, y=160)
-Button2 = Button(root, text='Audio downloader', font='calibri 14 bold', 
-bg='red', padx=2, command= audio_download)# The button to download the video in Audio format.
-Button2.place(x=310, y=160)
+Button2 = Button(root, text= ' Audio Download', font = 'calibri 10 bold', image = music_icon, command = audio_download, compound = LEFT, bg = '#8c0327', fg = 'white')
+Button2.place(x=320, y=160)
 
 
 root.mainloop()
